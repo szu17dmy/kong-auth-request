995f3eb15b3c23714e26358406af70a4602aa384
domain <nullptr@vip.qq.com>
2026-03-24 16:39:51 +0800

    :bug: remove unnecessary :new() and CustomHandler.super.XXX:(self)
    ref: https://developer.konghq.com/custom-plugins/handler.lua/
    
    "
      You don’t need to add a :new() method or call any of the CustomHandler.super.XXX:(self) methods.
    "

--- src/handler.lua
+++ src/handler.lua
@@ -2,17 +2,10 @@ local access = require "kong.plugins.kong-auth-request.access"
 
 local AuthRequestHandler = {
   VERSION  = "1.0.0",
-  PRIORITY = 10,
+  PRIORITY = 900,
 }
 
-AuthRequestHandler.PRIORITY = 900
-
-function AuthRequestHandler:new()
-  AuthRequestHandler.super.new(self, "kong-auth-request")
-end
-
 function AuthRequestHandler:access(conf)
-  AuthRequestHandler.super.access(self)
   access.execute(conf)
 end
 
